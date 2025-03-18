5867372a22efdc47270e576149e64a6cd664d295
WronglD <alissonmateus400@gmail.com>
2025-03-18 13:13:33 -0300

    Add files via upload

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,55 @@
+let nome = "";
+//HelloWorld
+
+function adicionarNome() {
+   nome = document.getElementById("nome").value;
+    if (nome == "") {
+        alert("Por favor, insira seu nome");
+    } else {
+        listaDeAmigos.push(nome);
+        document.getElementById("nome").value = "";
+        atualizarLista();
+    }
+}
+
+
+let listaDeAmigos = [];
+
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById("amigo").value;
+    if (nomeAmigo == "") {
+        alert("Por favor, é necessário um nome de um amigo para ser adicionado.");
+    } else {
+        listaDeAmigos.push(nomeAmigo);
+        document.getElementById("amigo").value ="";
+        atualizarLista();
+    }
+ 
+}
+
+function atualizarLista() {
+    let listaAtualizada = document.getElementById("listaAmigos");
+    listaAtualizada.innerHTML = "";
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let itemLista = document.createElement("li");
+        itemLista.innerHTML = listaDeAmigos[i];
+        listaAtualizada.appendChild(itemLista);
+    }
+}
+
+function sortearAmigo() {
+    if (listaDeAmigos.length == 0) {
+        alert("Por favor, antes de sortear insira o nome de um amigo.");
+        return;
+
+    }
+    let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+    let nomeSorteado = listaDeAmigos[indiceAleatorio];
+    if (nomeSorteado == nome) {
+        return;
+    }
+
+    document.getElementById("resultado").innerHTML = `Seu amigo secreto é: ${nomeSorteado}`;
+
+}  
